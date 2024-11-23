663d8da1b8428228937c92efb5d37762255836c3
zals023 <zalsgaming123@gmail.com>
2024-11-24 04:39:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '6f1c070a-7059-4f89-95ef-0d001151f057';
 
-const proxyIPs = ['104.248.145.216'];
+const proxyIPs = ['178.128.91.137'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
